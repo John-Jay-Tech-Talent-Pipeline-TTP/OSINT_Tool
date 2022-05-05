8c2060ac191a9e2080ce003396ff4c29792be4f4
PRINCE OSEI <81586911+PrxncE-LixH@users.noreply.github.com>
2022-05-05 19:35:10 +0000

    project description

--- main.py
+++ main.py
@@ -1,6 +1,51 @@
 from googlesearch import search
 import webbrowser
 import time
+import sys
+
+
+class colors:
+	red = "\033[1;31;40m"
+	green = "\033[1;32;40m"
+	blue = "\033[1;34;40m"
+	magneta = "\033[1;35;40m"
+title = ("""
+			▒┌───┬───┬──┬─┐▒┌┬────┐▒▒▒▒▒▒▒▒▒▒▒
+			▒│┌─┐│┌─┐├┤├┤│└┐││┌┐┌┐│▒▒▒▒▒▒▒▒▒▒▒
+			▒││▒││└──┐│││┌┐└┘├┘││└┘▒▒▒▒▒▒▒▒▒▒▒
+			▒││▒│├──┐│││││└┐││▒││▒▒▒▒▒▒▒▒▒▒▒▒▒
+			▒│└─┘│└─┘├┤├┤│▒│││▒││▒▒▒▒▒▒▒▒▒▒▒▒▒
+			▒└───┴───┴──┴┘▒└─┘▒└┘▒▒▒▒▒▒▒▒▒▒▒▒▒
+			▒▒▒▒▒▒▒▒▒▒▒▒▒┌───┬───┬───┬┐┌─┬───┐
+			▒▒▒▒▒▒▒▒▒▒▒▒▒└┐┌┐│┌─┐│┌─┐│││┌┤┌─┐│
+			▒▒▒▒▒▒▒▒▒▒▒▒▒▒│││││▒││└─┘│└┘┘│└──┐
+			▒▒▒▒▒▒▒▒▒▒▒▒▒▒│││││▒││┌┐┌┤┌┐│└──┐│
+			▒▒▒▒▒▒▒▒▒▒▒▒▒┌┘└┘│└─┘│││└┤││└┤└─┘│
+			▒▒▒▒▒▒▒▒▒▒▒▒▒└───┴───┴┘└─┴┘└─┴───┘
+""")
+
+for col in title:
+	print(colors.blue + col, end= "")
+	sys.stdout.flush()
+	time.sleep(0.0025)
+	
+description = "\n The tool uses advanced searching operators in the Goggle search engine to find specfic words or phrases which helps in finding any vulnerable web applications. It simplifies Google Dorking commands for the user and provides straightforward anaylsis of the information. Different information such as contact information, login credentials, files on a website, open ports, vulnerability of a specific website, and many more juicy stuff can be found online through google dorking. This tool allows you to be able to access that information wihout knowing any google dorking commands or strings.  \n"
+
+for col in description:
+	print(colors.magneta + col, end = "")
+	sys.stdout.flush()
+	time.sleep(0.01)
+	
+disclaimer = " \n This is for educational purposes and user should use it at their own risk. Some of the anaylsis might not be precise and CAUTION is advised when opening the links found through the results. \n\n "
+
+for col in disclaimer:
+	print(colors.red + col, end = "")
+	sys.stdout.flush()
+	time.sleep(0.01)
+	
+print("\033[1;32;40mHave fun hakcing *Hacker music in background*\n\n")
+
+
 
 
 # uses console or default browser to display results.
